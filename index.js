52e32c592b0b35af70b59733353aea958aa156b6
lanell1 <lanelllopez@pursuit.org>
2023-12-12 06:49:38 -0500

    complete-01-13-even-or-odd

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const numbers = [12, 25, 42, 53, 60];
 numbers.forEach(number => {
   let result = '';
 
+  if (number % 2 === 0) {
+    result = 'Even number';
+  } else if (number % 2 === 1) {
+    result = 'Odd number';
+  }
+
   // Write your if-else statements here to determine even or odd
 
   console.log(`Number ${number} is ${result}`);
